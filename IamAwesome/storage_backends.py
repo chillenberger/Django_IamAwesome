9f66d1aeca2e48ea74975c65b61a4af909e9f1c7
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-05-19 19:27:51 -0400

    attempts to add s3 to heroku

--- IamAwesome/storage_backends.py
+++ IamAwesome/storage_backends.py
@@ -1,4 +1,4 @@
-#this file is to support saving user uploaded photos to aws using boto3 
+#this file is to support saving user uploaded photos to aws using boto3
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaStorage(S3Boto3Storage):
