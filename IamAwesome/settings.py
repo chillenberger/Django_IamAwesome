e5391df3336e07a704e489a26d3294b0193df351
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-05-19 14:37:21 -0400

    try 2 to use aws in heroku

--- IamAwesome/settings.py
+++ IamAwesome/settings.py
@@ -153,7 +153,7 @@ DATABASES['default'].update(db_from_env)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# old code for white noise static library STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
