b5c3d2ce91ac96f1005576807dc836e60230bffb
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-05-20 12:28:03 -0400

    accessing files s3 update

--- catalog/forms.py
+++ catalog/forms.py
@@ -50,9 +50,9 @@ class NewStoryForm(forms.ModelForm):
 
         cropped_image = img.crop((left, top, right, bottom))
         print ( 'photo cropped ran')
-        print ( story.photo.path )
-        print ( story.photo.key )
-        cropped_image.save( story.photo.key)
+        print ( story.photo.name )
+        # print ( story.photo.key )
+        # cropped_image.save( story.photo.key)
         print("Save attempted")
 
         return story
