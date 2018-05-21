d0a61c18ec9777167a4d87a360a86895bb52b9fb
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-05-20 20:25:42 -0400

    from shell program

--- catalog/forms.py
+++ catalog/forms.py
@@ -52,13 +52,11 @@ class NewStoryForm(forms.ModelForm):
         print ( 'photo cropped ran')
         print ( story.photo.name )
 
-        story.photo = cropped_image
-        # cropped_image.save( "media/" + story.photo.name )
-        # f = cropped_image.read()
+        output = io.BytesIO()
+        cropped_image.save( output )
         s3 = boto3.client('s3')
-        # s3.Object('iamawesomepicturebucket2', s3PhotoFile).put(Body=open( "media/" + story.photo.name, 'rb'))
 
-        # s3.upload_fileobj( cropped_image.tobitmap , "iamawesomepicturebucket2", story.photo.name )
+        s3.upload_fileobj( output , "iamawesomepicturebucket2", story.photo.name )
 
         print("Save attempted")
 
