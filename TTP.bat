3f0838d7453ecd5278601224784e3e3856ee7ead
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-06-10 11:33:03 -0400

    Minify wide and narrow css files

--- TTP.bat
+++ TTP.bat
@@ -3,6 +3,8 @@ SET /P commit=State Changes to Code:
 IF "%commit%"==""  GOTO End
 git commit -m "%commit%"
 git push origin master
+SET /P pushToHeroku=Push to Heroku?(y/N):
+IF "%pushToHeroku%"!="Y" GOTO End
 git push heroku master
 heroku logs -t
 :End
