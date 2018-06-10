187bca9c940983115def8eb99590af73a639ea68
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-06-10 10:14:56 -0400

    commit

--- TTP.bat
+++ TTP.bat
@@ -1,5 +1,7 @@
 git add -A
-git commit -m "from shell program"
+SET P/ commit = State Changes to Code:
+If commit == "" commit = "no commit provided"
+git commit -m commit
 git push origin master
 git push heroku master
 heroku logs -t
