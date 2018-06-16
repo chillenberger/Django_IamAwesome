dbd6515a281c88460e3fdacac7a36f9798df3a04
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-06-16 16:48:04 -0400

    testing images and fixing html for seo

--- TTP.bat
+++ TTP.bat
@@ -3,7 +3,7 @@ SET /P commit=State Changes to Code:
 IF "%commit%"==""  GOTO End
 git commit -m "%commit%"
 git push origin master
-SET /P pushToHeroku=Push to Heroku?(y/N):
+SET /P pushToHeroku=Push to Heroku?(Y/N):
 IF "%pushToHeroku%"!="Y" GOTO End
 git push heroku master
 heroku logs -t
