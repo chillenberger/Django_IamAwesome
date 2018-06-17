41a7c1b03e1f7cd295d0f0946d555ab00b963196
chillenberger <39170265+chillenberger@users.noreply.github.com>
2018-06-17 11:11:28 -0400

    return to previous version for images

--- TTP.bat
+++ TTP.bat
@@ -4,7 +4,7 @@ IF "%commit%"==""  GOTO End
 git commit -m "%commit%"
 git push origin master
 SET /P pushToHeroku=Push to Heroku?(Y/N):
-IF [NOT] "%pushToHeroku%"=="Y" GOTO End
+IF "%pushToHeroku%"=="N" GOTO End
 git push heroku master
 heroku logs -t
 :End
